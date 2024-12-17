63450a35d8269ee94b50ad662d708c602fd6dca9
Harini R <1816775@tcs.com>
2024-12-17 20:11:18 +0530

    Updated assignments

--- Selenium_Testing/src/basicprogram/Automateme_81.java
+++ Selenium_Testing/src/basicprogram/Automateme_81.java
@@ -1,7 +1,9 @@
 package basicprogram;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Automateme_81 {
 	public static void main(String[] args) {
@@ -15,6 +17,20 @@ public class Automateme_81 {
 		driver.findElement(By.id("Present-Address")).sendKeys("No 2 ,9th street,New Colony,Adambakkam,Chennai 88.");
 		driver.findElement(By.id("Permanent-Address")).sendKeys("No 2 ,9th street,New Colony,Adambakkam,Chennai 88.");
 		driver.findElement(By.id("Pincode")).sendKeys("600088");
+		WebElement w = driver.findElement(By.id("Skills"));
+		Select s = new Select(w);
+		s.selectByVisibleText("Technical Skills");
+		WebElement w1 = driver.findElement(By.id("technicalskills"));
+		Select s1 = new Select(w1);
+		s1.selectByVisibleText("Core Java");
+		WebElement w2 = driver.findElement(By.id("Country"));
+		Select s2 = new Select(w2);
+		s2.selectByIndex(8);
+		WebElement w3 = driver.findElement(By.id("Relegion"));
+		Select s3 = new Select(w3);
+		s3.selectByValue("Hindu");
+		
+		
 		
 		
 	}
--- Selenium_Testing/src/basicprogram/Loginamazon.java
+++ Selenium_Testing/src/basicprogram/Loginamazon.java
@@ -1,7 +1,21 @@
 package basicprogram;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Loginamazon {
 public static void main(String[] args) {
-	System.out.println("Hello");
+	ChromeDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.amazon.in/");
+	driver.findElement(By.id("nav-link-accountList")).click();
+	driver.findElement(By.id("ap_email")).sendKeys("9444863553");
+	driver.findElement(By.id("continue")).click();
+	driver.findElement(By.id("ap_password")).sendKeys("abcde@12345");
+	driver.findElement(By.id("signInSubmit")).click();
+	//using linktext to click on customer service
+	driver.findElement(By.linkText("Customer Service")).click();
+	driver.findElement(By.partialLinkText("Car")).click();
+	
 }
 }
